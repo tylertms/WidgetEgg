7c1ebd01ec353280e94ada03486f4cbe1b138a00
Tyler Smith <tylertms@icloud.com>
2024-06-11 16:48:41 -0400

    Add watchOS target and adjust app views for cross-platform compatibility

---
+++ WidgetEggWatch/WidgetEggWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  WidgetEggWatchApp.swift
+//  WidgetEggWatch Watch App
+//
+//  Created by Tyler on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetEggWatch_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ScrollView {
+                HomeView()
+            }
+        }
+    }
+}
