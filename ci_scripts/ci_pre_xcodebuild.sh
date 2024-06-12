0aaf7774816709eed407f506abe0c56d72290068
Tyler Smith <tylertms@icloud.com>
2024-06-12 14:53:04 -0400

    Separate app and extension plists

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -11,11 +11,12 @@ if [ -f ~/.bash_profile ]; then
 fi
 
 # Define the path for the plist file using the SRCROOT environment variable
-PLIST_PATH="${SRCROOT}/MissionInfo/Info.plist"
+APP_PLIST_PATH="${SRCROOT}/WidgetEgg/Info.plist"
+EXT_PLIST_PATH="${SRCROOT}/WidgetEgg/ExtensionInfo.plist"
 
 echo "BUNDLING WITH KEY $KEY"
 
-cat > "$PLIST_PATH" << EOF
+cat > "$APP_PLIST_PATH" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -29,8 +30,34 @@ cat > "$PLIST_PATH" << EOF
 		<key>INDEX</key>
 		<string>$INDEX</string>
 	</dict>
+	<key>ITSAppUsesNonExemptEncryption</key>
+	<false/>
 </dict>
 </plist>
 EOF
 
-echo "PLIST CREATED AT $PLIST_PATH"
+cat > "$EXT_PLIST_PATH" << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>LSEnvironment</key>
+	<dict>
+		<key>KEY</key>
+		<string>$KEY</string>
+		<key>MARKER</key>
+		<string>$MARKER</string>
+		<key>INDEX</key>
+		<string>$INDEX</string>
+	</dict>
+	<key>NSExtension</key>
+	<dict>
+		<key>NSExtensionPointIdentifier</key>
+		<string>com.apple.widgetkit-extension</string>
+	</dict>
+</dict>
+</plist>
+EOF
+
+echo "APP PLIST CREATED AT $APP_PLIST_PATH"
+echo "EXT PLIST CREATED AT $EXT_PLIST_PATH"
