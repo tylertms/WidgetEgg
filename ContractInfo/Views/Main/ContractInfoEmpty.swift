79611d992bd20f42698571c170a68c7a0d23e4bf
Tyler Smith <tylertms@icloud.com>
2025-09-09 14:44:56 -0400

    Create empty widget view (no contracts)

---
+++ ContractInfo/Views/Main/ContractInfoEmpty.swift
@@ -0,0 +1,39 @@
+//
+//  ContractInfoEmpty.swift
+//  WidgetEgg
+//
+//  Created by Tyler on 9/9/25.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct ContractInfoEmpty: View {
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 8)
+                .frame(height: 16)
+                .foregroundStyle(.gray.opacity(0.25))
+                .padding(.horizontal, -15)
+            
+            VStack {
+                ForEach(0..<2) { index in
+                    RoundedRectangle(cornerRadius: 5)
+                        .frame(height: 10)
+                        .foregroundStyle(.gray.opacity(0.18))
+                        .padding(.horizontal, -15)
+                }
+            }
+            .padding(.bottom)
+            
+            VStack {
+                ForEach(0..<3) { index in
+                    RoundedRectangle(cornerRadius: 5)
+                        .frame(height: 10)
+                        .foregroundStyle(.gray.opacity(0.18))
+                        .padding(.horizontal, -15)
+                }
+            }
+        }
+    }
+}
